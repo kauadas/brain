737f2c55dfcb683b5de68da054cdee7e78b86fe3
kauã stocker <kauazinhodaweb@gmail.com>
2025-01-09 10:29:39 -0300

    adição do .gitignore e correção no main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from data.modules.interface import Aplicativo
+from pathlib import Path
+from os import chdir
 
+file = Path(__file__)
+chdir(file.parent)
 
 if __name__ == '__main__':
     app = Aplicativo()
