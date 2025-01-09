401c9496b4ca7c5f641871320d3bcadde5d940ed
kauã stocker <kauazinhodaweb@gmail.com>
2025-01-09 10:45:33 -0300

    v 1.0.0 coisas de segurança

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from data.modules.interface import Aplicativo
 from pathlib import Path
-from os import chdir
+from os import chdir, path, mkdir
 
 file = Path(__file__)
 chdir(file.parent)
+
+if not path.exists("data/json/quadros"):
+    mkdir("data/json/quadros")
+
+if not path.exists("data/json/configs"):
+    mkdir("data/json/configs")
 
 if __name__ == '__main__':
     app = Aplicativo()
