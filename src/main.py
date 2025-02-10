b49270c79cf368740e78e39e4298fdf6c93581f8
kauã stocker <kauazinhodaweb@gmail.com>
2025-02-10 16:11:23 -0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ from data.modules.interface import Aplicativo
 from data.modules.utils import generate_paths
 from pathlib import Path
 from os import chdir, path, mkdir
+from sys import argv
 
-file = Path(__file__)
+if len(argv) > 1:
+    file = Path(argv[1])
+
+else:
+    file = Path(__file__)
+    
 chdir(file.parent)
 print(file.parent)
 
